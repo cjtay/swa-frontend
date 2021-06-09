b6c3164815335e0de5672cdc59b6de4dc78c249a
Tay CJ <cjtay@outlook.sg>
2021-06-09 16:13:13 +0800

    added error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
-import Seo from "../components/seo"
+
+// import SEO from "../components/layout/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
+    {/* <SEO title="404: Not found" /> */}
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">
+      <h2 className="inline-block px-3 py-2 mt-5 border-2 rounded text-swa-1 border-swa-1">
+        Go back to home page
+      </h2>
+    </Link>
   </Layout>
 )
 
